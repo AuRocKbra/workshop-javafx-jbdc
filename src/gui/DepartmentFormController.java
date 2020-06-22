fd7913959f85188da2256d668982d62a425cbb53
AuRocKbra <4haurock@gamil.com>
2020-06-22 11:10:32 -0300

    Implementado função de salvar em departmento

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -2,18 +2,25 @@ package gui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
+import db.DBException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable{
 	
 	private Department entity;
+	
+	private DepartmentService service;
 	
 	@FXML
 	private TextField txtId;
@@ -31,13 +38,32 @@ public class DepartmentFormController implements Initializable{
 	private Button btCancel;
 	
 	@FXML
-	public void onBtSaveAction() {
-		System.out.println("press Save!");
+	public void onBtSaveAction(ActionEvent event) {
+		if(entity==null) {
+			throw new IllegalStateException("Objeto departamento não instanciado!");
+		}
+		if(service ==null) {
+			throw new IllegalStateException("Objeto serviço não instanciado!");
+		}
+		try {
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+		}catch(DBException e) {
+			Alerts.showAlertas("Error", "Operação de salvar dados",e.getMessage(),AlertType.ERROR);
+		}
+		gui.util.Utils.currentStage(event).close();
 	}
 	
+	private Department getFormData() {
+		Department obj = new Department();
+		obj.setId(gui.util.Utils.tryParseToInt(txtId.getText()));
+		obj.setNome(txtNome.getText());
+		return obj;
+	}
+
 	@FXML
-	public void onBtCancelAction(){
-		System.out.println("press Cancel!");
+	public void onBtCancelAction(ActionEvent event){
+		Utils.currentStage(event).close();
 	}
 	
 	@Override
@@ -65,5 +91,9 @@ public class DepartmentFormController implements Initializable{
 			txtId.setText(String.valueOf(entity.getId()));
 		}
 		txtNome.setText(entity.getNome());
+	}
+	
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
 	}
 }
